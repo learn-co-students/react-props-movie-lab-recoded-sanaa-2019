3ce782da329bda131fa3437e50b395f30e45972d
Sarah Alafif <sar1997yah@gmail.com>
2019-12-17 14:15:08 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -13,7 +13,7 @@ export default class CardBack extends Component {
   generateRatingElement = () => {
    if (this.props.IMDBRating === null) {
        return <h4>{'No rating found'}</h4>
-   else {	  
+   }else {	  
     return <img src={imgMapper[this.props.IMDBRating]} />
       }
   }
